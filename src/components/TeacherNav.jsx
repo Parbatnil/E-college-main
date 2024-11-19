4cbfcaed685f692d41821196cd50b5010768cac1
Parbat Nil Bera <parbatnilbera743@gmail.com>
2024-11-19 10:16:42 +0530

    wpwow

--- src/components/TeacherNav.jsx
+++ src/components/TeacherNav.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import logo from "../../public/logo.png";
-import { FaTimes, FaBars, FaSearch } from "react-icons/fa";
+import { FaTimes } from "react-icons/fa";
+import { FaBars } from "react-icons/fa6";
 import { useNavigate } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
 import axios from "axios";
 
 const TeacherNav = () => {
@@ -34,6 +36,7 @@ const TeacherNav = () => {
         console.error(err);
       }
     };
+
     fetchData();
   }, []);
 
@@ -58,7 +61,7 @@ const TeacherNav = () => {
   return (
     <div>
       {/* Desktop Menu */}
-      <nav className="lg:flex hidden justify-between items-center w-full h-16 bg-blue-900 text-white">
+      <nav className="lg:flex hidden justify-between items-center w-full h-16 bg-slate-200">
         {/* Logo */}
         <div className="flex justify-center items-center gap-3">
           <img
@@ -68,59 +71,59 @@ const TeacherNav = () => {
             alt="logo"
           />
           <div
-            className="text-white pb-3 font-semibold text-base cursor-pointer"
+            className="text-green-700 pb-3 font-semibold text-base cursor-pointer"
             onClick={() => navigate("/teachercourseok@24")}
           >
             E-College
           </div>
         </div>
         {/* Menu Items */}
-        <div className="flex justify-center items-center gap-5 text-gray-300">
+        <div className="flex justify-center items-center gap-5 text-gray-600">
           <div
-            className="hover:text-white cursor-pointer"
+            className="hover:text-black cursor-pointer"
             onClick={() => navigate("/")}
           >
             Home
           </div>
           <div
-            className="hover:text-white cursor-pointer"
+            className="hover:text-black cursor-pointer"
             onClick={() => navigate("/attendence")}
           >
             Attendance
           </div>
           <div
-            className="hover:text-white cursor-pointer"
-            onClick={() => navigate("/teacher-marks")}
+            className="hover:text-black cursor-pointer"
+            onClick={() => navigate("/mark")}
           >
             Marks
           </div>
           <div
-            className="hover:text-white cursor-pointer"
+            className="hover:text-black cursor-pointer"
             onClick={() => navigate("/managementcontacts")}
           >
             Management Contacts
           </div>
         </div>
-        {/* Login Button */}
+        {/* Login */}
         <div className="flex justify-center items-center gap-3 m-2">
           {log ? (
             <button
-              className="bg-blue-600 text-white py-2 px-4 rounded-3xl"
+              className="bg-orange-500 text-white py-2 px-4 rounded-3xl"
               onClick={getlog}
             >
               LogIn
             </button>
           ) : (
             <button
-              className="bg-blue-600 text-white py-2 px-4 rounded-3xl"
+              className="bg-orange-500 text-white py-2 px-4 rounded-3xl"
               onClick={update}
             >
               LogOut
             </button>
           )}
-          {/* Profile Initial */}
+          {/* Change */}
           <div
-            className="cursor-pointer rounded-full h-10 w-10 bg-indigo-700 text-white flex items-center justify-center"
+            className="cursor-pointer rounded-full h-10 w-10 bg-purple-700 text-white pl-3.5 pt-2"
             onClick={() => navigate(`/dashboard/${check}`)}
           >
             {l}
@@ -129,19 +132,18 @@ const TeacherNav = () => {
       </nav>
 
       {/* Mobile Menu */}
-      {/* Mobile Menu */}
-      <div className="rounded-lg max-h-12 lg:hidden bg-blue-900 text-white fixed w-full z-50 top-0">
+      <div className="lg:hidden bg-slate-200">
         <div className="flex items-center justify-between">
-          <button
-            className="focus:outline-none lg:hidden"
-            onClick={toggleMobileMenu}
-          >
-            {isMobileMenuOpen ? (
-              <FaTimes className="m-2 h-6 w-5" />
-            ) : (
-              <FaBars className="m-2 h-6 w-5" />
-            )}
-          </button>
+          <div className="flex items-center gap-3">
+            {/* Toggle Mobile Menu */}
+            <button className="focus:outline-none" onClick={toggleMobileMenu}>
+              {isMobileMenuOpen ? (
+                <FaTimes className="m-2 h-6 w-5" />
+              ) : (
+                <FaBars className="m-2 h-6 w-5" />
+              )}
+            </button>
+          </div>
 
           <div>
             <a href="#" className="flex justify-center items-center">
@@ -153,52 +155,57 @@ const TeacherNav = () => {
                 alt="logo"
               />
               <div
-                className="text-white pb-2 font-semibold text-sm cursor-pointer"
+                className="text-green-700 pb-2 font-semibold text-sm cursor-pointer"
                 onClick={() => navigate("/teachercourseok@24")}
               >
                 E-College
               </div>
             </a>
           </div>
-          <div
-            className="cursor-pointer rounded-full h-6 w-6 bg-indigo-700 text-white flex items-center justify-center"
-            onClick={() => navigate(`/dashboard/${check}`)}
-          >
-            {l}
+
+          <div>
+            <div
+              className="cursor-pointer rounded-full h-6 w-6 bg-purple-700 text-white pl-1.5 mr-1"
+              onClick={() => navigate(`/dashboard/${check}`)}
+            >
+              {l}
+            </div>
           </div>
         </div>
+
+        {/* Mobile Menu Items */}
         {isMobileMenuOpen && (
-          <ul className="mx-2 z-50 p-2 mt-2 flex flex-col gap-4 text-gray-300 bg-blue-900 py-5 rounded-md">
+          <ul className="mx-2 p-2 mt-2 flex flex-col gap-4 text-gray-600 bg-slate-200 py-5 rounded-md">
             <li
-              className="hover:text-white cursor-pointer"
+              className="hover:text-black cursor-pointer"
               onClick={() => navigate("/")}
             >
               Home
             </li>
             <li
-              className="hover:text-white cursor-pointer"
+              className="hover:text-black cursor-pointer"
               onClick={() => navigate("/attendence")}
             >
               Attendance
             </li>
             <li
-              className="hover:text-white cursor-pointer"
+              className="hover:text-black cursor-pointer"
               onClick={() => navigate("/mark")}
             >
               Marks
             </li>
             <li
-              className="hover:text-white cursor-pointer"
+              className="hover:text-black cursor-pointer"
               onClick={() => navigate("/managementcontacts")}
             >
               Management Contacts
             </li>
             {log ? (
-              <li className="hover:text-white cursor-pointer" onClick={getlog}>
+              <li className="hover:text-black cursor-pointer" onClick={getlog}>
                 LogIn
               </li>
             ) : (
-              <li className="hover:text-white cursor-pointer" onClick={update}>
+              <li className="hover:text-black cursor-pointer" onClick={update}>
                 LogOut
               </li>
             )}
